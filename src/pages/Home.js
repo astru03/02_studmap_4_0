a701ff389368ef35d60b4dcc2a86f1f33d6ac582
Andreas Struffert-Froböse <a_stru03@uni-muenster.de>
2025-01-16 15:11:21 +0100

    add leaflet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,24 @@
 import React from "react";
-import Sidebar from "../components/Sidebar";
-import "./Home.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Home = () => {
   return (
-    <div className="home">
-      <Sidebar />
-      <div className="content">
-        <h1>Willkommen auf der Startseite!</h1>
-        <p>Das ist die Hauptansicht deiner Webseite.</p>
-      </div>
+    <div className="map-container">
+      <MapContainer
+        center={[51.505, -0.09]} // Startkoordinaten (z.B. London)
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            Hier ist ein Beispiel-Popup auf der Karte.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
